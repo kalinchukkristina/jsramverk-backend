d223cc00a68d0736e87e5abff9d093c6291e3c7f
TinkzBot <zakaria.ziadi92@gmail.com>
2023-09-19 20:41:00 +0200

    Update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.disable("x-powered-by");
 
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000/", "http://www.student.bth.se/~krka21/editor/],
     methods: ["GET", "POST"],
   },
 });
